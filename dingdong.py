ba84c6af9b8ffe2dc972735607a9f5e8dc3bbd53
Jaromir Latal <latal.jaromir@gmail.com>
2016-10-15 19:05:51 +0200

    finished Twilio ding-dong

---
+++ dingdong.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from twilio import twiml
+
+app = Flask(__name__)
+
+
+@app.route("/record", methods=['GET', 'POST'])
+def record():
+    """Returns TwiML which prompts the caller to record a message"""
+    # Start our TwiML response
+    response = twiml.Response()
+    # Use <Say> to give the caller some instructions
+    response.say('Hello. Please leave a message after the beep.')
+    # Use <Record> to record the caller's message
+    response.record(transcribe=True, transcribeCallback='/calculate')
+    # End the call with <Hangup>
+    response.hangup()
+
+    return str(response)
+
+
+@app.route('/calculate', methods=['GET', 'POST'])
+def callback():
+    text = request.form['TranscriptionText']
+    return text
+
+if __name__ == "__main__":
+    app.run()
