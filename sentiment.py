ba14c57884b90dba84875b4e731c7e5d43b2acd0
Ewil <ewildawe@gmail.com>
2016-10-15 20:11:50 +0100

    Improved spam filter

--- sentiment.py
+++ sentiment.py
@@ -1,18 +1,26 @@
 from textblob import TextBlob
-from collections import namedtuple
 import operator
 
-
-__constants__ = namedtuple("Constants", ['polarity', 'subjectivity'])(0, 0.2)
 
 # Yes, I know this is bad practice - what can you do? -- No time to write a proper classifier
 SCORE_MIN = 10
 _temp_wordlist_ = {
     'rape': 10,
     'molest': 10,
+    'attack': 10,
+    'harass': 10,
+    'leering': 10,
+    'whistl': 10,
+    'inadequate': 10,
+    'exploit': 10,
+    'sex': 10,
+    'stop': 3,
+    'hurt': 10,
     'report': 8,
     'help': 6,
     'what': -1,
+    'away': -5,
+    'fatal': -10,
     'lol': -10,
     'wtf': -10,
     'gay': -10,
@@ -27,15 +35,18 @@ def _tuplesum(tuple_list):
 
 
 def analyze(text):
-    blob = TextBlob(text)
-    polarity, subjectivity, score = 0, 0, 1
+    blob = TextBlob(text.lower())
+    polarity, subjectivity, score = 0, 0, 0
     for sentence in blob.sentences:
         polarity += sentence.polarity
         subjectivity += sentence.subjectivity
-    for word in blob.words:
+    for word in set(blob.words):
         for word_test in _temp_wordlist_:
             if word_test in word:
                 score += _temp_wordlist_[word_test]
-    return polarity < __constants__.polarity and subjectivity > __constants__.subjectivity and score >= SCORE_MIN
+    print((score * (1 - polarity) * (1 + subjectivity)))
+    return (score * (1 - polarity) * (1 + subjectivity)) > SCORE_MIN
 
 
+while True:
+    print(analyze(input(">> ")))
